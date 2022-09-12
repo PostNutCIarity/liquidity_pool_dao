250ca961a04c0b28dd782906f6a32003617edf46
jake-rdxworks <jakephucmai@gmail.com>
2022-09-12 10:12:36 -0400

    commit

--- target/debug/deps/lib-e82b8c5bfbf09197.d
+++ target/debug/deps/lib-e82b8c5bfbf09197.d
@@ -1,7 +1,7 @@
-/Users/jakemai/Documents/GitHub/liquidity_pool_dao/liquidity_pool_dao/target/debug/deps/lib-e82b8c5bfbf09197.rmeta: tests/lib.rs
+/Users/jakemai/Documents/GitHub/liquidity_pool_dao/target/debug/deps/lib-e82b8c5bfbf09197.rmeta: tests/lib.rs
 
-/Users/jakemai/Documents/GitHub/liquidity_pool_dao/liquidity_pool_dao/target/debug/deps/lib-e82b8c5bfbf09197.d: tests/lib.rs
+/Users/jakemai/Documents/GitHub/liquidity_pool_dao/target/debug/deps/lib-e82b8c5bfbf09197.d: tests/lib.rs
 
 tests/lib.rs:
 
-# env-dep:CARGO_MANIFEST_DIR=/Users/jakemai/Documents/GitHub/liquidity_pool_dao/liquidity_pool_dao
+# env-dep:CARGO_MANIFEST_DIR=/Users/jakemai/Documents/GitHub/liquidity_pool_dao
